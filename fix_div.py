c07a3fcd458910cb7b6407b2fbc1e254032b7a22
sahebbiswas <6592033+sahebbiswas@users.noreply.github.com>
2026-08-29 16:24:08 +0000

    feat: Add PointerSubtractionSizeRule (CGULL-046) for CWE-469

--- fix_div.py
+++
@@ -1,5 +0,0 @@
-with open('cgull/rules/types_and_arrays.py', 'r') as f:
-    content = f.read()
-
-# Since I messed up the `self.create_issue` everywhere because I used `sed`-like replacements improperly earlier,
-# The easiest way is to restore the file to the state before my edits.
