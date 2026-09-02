ba0c1e46be7b78c43dddaa5a1e585ef2445def1b
Saheb <sahebbiswas@gmail.com>
2026-09-02 13:35:05 -0600

    Wire CGULL-007 constant bounds evaluator

--- cgull/rules/types_and_arrays/__init__.py
+++ cgull/rules/types_and_arrays/__init__.py
@@ -5,7 +5,7 @@ Types and arrays rules for C-GULL Static Analyzer.
 from .variable_length_arrays import VariableLengthArraysRule
 from .incorrect_pointer_scaling import IncorrectPointerScalingRule
 from .sizeof_on_pointer import SizeofOnPointerRule
-from .array_index_out_of_bounds import ArrayIndexOutOfBoundsRule
+from .array_index_constant_bounds import ArrayIndexOutOfBoundsRule
 from .arithmetic_integer_overflow import ArithmeticIntegerOverflowRule
 from .bitwise_operations_on_signed_integers import BitwiseOperationsOnSignedIntegersRule
 from .use_of_magic_numbers import UseOfMagicNumbersRule
