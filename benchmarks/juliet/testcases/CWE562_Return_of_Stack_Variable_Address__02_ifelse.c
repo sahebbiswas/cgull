7aa4e242243bedb5d64206f4dc8cc02323a17f39
Saheb <sahebbiswas@gmail.com>
2026-09-02 16:43:54 -0600

    Add CWE-562 Juliet ifelse fixture

---
+++ benchmarks/juliet/testcases/CWE562_Return_of_Stack_Variable_Address__02_ifelse.c
@@ -0,0 +1,22 @@
+void CWE562_Return_of_Stack_Variable_Address__02_ifelse_bad(void)
+{
+    int flag = 1;
+    int local = 1;
+    if (flag) {
+        return &local;
+    } else {
+        return;
+    }
+}
+
+void CWE562_Return_of_Stack_Variable_Address__02_ifelse_good(void)
+{
+    int flag = 1;
+    static int local = 1;
+    if (flag) {
+        (void)local;
+        return;
+    } else {
+        return;
+    }
+}
