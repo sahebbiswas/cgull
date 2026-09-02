0e4ad25009910f215314698b2d7689fca5b15632
Saheb <sahebbiswas@gmail.com>
2026-09-02 17:26:42 -0600

    Fix CWE-562 fixtures to use pointer return types

--- benchmarks/juliet/testcases/CWE562_Return_of_Stack_Variable_Address__01_baseline.c
+++ benchmarks/juliet/testcases/CWE562_Return_of_Stack_Variable_Address__01_baseline.c
@@ -1,12 +1,11 @@
-void CWE562_Return_of_Stack_Variable_Address__01_baseline_bad(void)
+int *CWE562_Return_of_Stack_Variable_Address__01_baseline_bad(void)
 {
     int local = 1;
     return &local;
 }
 
-void CWE562_Return_of_Stack_Variable_Address__01_baseline_good(void)
+int *CWE562_Return_of_Stack_Variable_Address__01_baseline_good(void)
 {
     static int local = 1;
-    (void)local;
-    return;
+    return &local;
 }
