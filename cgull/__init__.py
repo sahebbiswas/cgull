7ce9ab8197f4b15a7173058d86d8bb951a1406b7
Saheb <sahebbiswas@gmail.com>
2026-09-02 11:03:14 -0600

    Bump version to 0.9.27

--- cgull/__init__.py
+++ cgull/__init__.py
@@ -3,7 +3,7 @@ C-GULL: Code Guardian for Unchecked Logic & Leaks
 A lightweight & AST-capable C Static Code Security Analyzer.
 """
 
-__version__ = "0.9.26"
+__version__ = "0.9.27"
 __author__ = "Saheb Biswas"
 
 from .logging_config import configure_logging, TRACE_LEVEL_NUM
